9f7e35366df6abba59fba3d02ccad114c1975cad
BirukL137 <buraf13@gmail.com>
2025-11-12 19:29:40 +0300

    Main Component

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+import "./styles/reset.css";
+import "./styles/tokens.css";
+
+import { ThemeProvider } from "./context/ThemeContext.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
