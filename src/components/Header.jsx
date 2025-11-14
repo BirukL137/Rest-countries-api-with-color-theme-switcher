99acfb8ced07351315c9d08fa5c0dcc19c80ecd2
BirukL137 <buraf13@gmail.com>
2025-11-14 16:25:35 +0300

    Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,25 @@
 import { useContext } from "react";
-import "./Header.css";
+import "../styles/Header.css";
+import { Sun, Moon } from "lucide-react";
 import { ThemeContext } from "../context/ThemeContext";
 
 const Header = () => {
-  const { toggleTheme } = useContext(ThemeContext);
+  const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
     <header className="header">
       <div className="header__wrapper">
         <h1 className="header__title">Where in the world?</h1>
         <button
-          className="header__theme-button"
+          className="header__toggle"
           onClick={toggleTheme}
           aria-label="Toggle dark mode"
         >
-          <span className="header__icon" aria-hidden="true">
-            🌙
+          <span className="header__toggle-icon" aria-hidden="true">
+            {/* 🌙 */}
+            {theme === "dark" ? <Sun fill="yellow" /> : <Moon />}
           </span>
-          <span>Dark Mode</span>
+          <span>{theme === "dark" ? "Light Mode" : "Dark Mode"}</span>
         </button>
       </div>
     </header>
