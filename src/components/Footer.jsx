4285922fedb4a56ca7311b3c5b1002e8d1e7dd12
BirukL137 <buraf13@gmail.com>
2025-11-14 19:46:49 +0300

    Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import "../styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer__credit">
+        Designed &amp; built with care by <span>Biruk Lemma</span> 🌿
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
