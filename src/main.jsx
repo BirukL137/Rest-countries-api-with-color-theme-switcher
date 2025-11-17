bcbda340ab06be437b4cddf8435cafdede94c4b3
BirukL137 <buraf13@gmail.com>
2025-11-17 13:58:21 +0300

    Main Component

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,8 @@ import App from "./App.jsx";
 import "./styles/reset.css";
 import "./styles/tokens.css";
 
-import { ThemeProvider } from "./context/ThemeContext.jsx";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>
 );
